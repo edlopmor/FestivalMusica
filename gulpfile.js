c9d08e6d833b22227674e4a7813b5ecb876bb78a
Edgar López <edlopmor@hotmail.es>
2021-10-11 09:19:07 +0200

    Antes de implementar el video

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 const {src,dest,watch} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 
 
 
 function compilarCSS (done){
     //Identificar el archivo sass a compilar
     src('src/scss/**/*.scss')
+    //En caso de haber errores no detiene el workflow
+    .pipe(plumber())
     //Compilar
     .pipe( sass() )
     //Almacenarla en el disco duro
